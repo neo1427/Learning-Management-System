322a9dbcc9e71c1b07b09319018d80afebf81107
karthikeya <venkata.karthikeya@iamneo.ai>
2024-04-05 14:50:10 +0530

    Updates the errors

--- src/main/java/com/iamneo/springboot/LMS/Service/TestUpdateService.java
+++ src/main/java/com/iamneo/springboot/LMS/Service/TestUpdateService.java
@@ -1,4 +1,4 @@
-package com.iamneo.springboot.LMS.Service;
+package com.iamneo.springboot.LMS.service;
 
 import com.iamneo.springboot.LMS.model.Test;
 import com.iamneo.springboot.LMS.repository.TestResultRepository;
