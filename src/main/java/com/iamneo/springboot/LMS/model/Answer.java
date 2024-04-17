a544d86df0cf0e53e43f695695a2e6fd98832132
Vamsi Giri <vamsi.giri@imaneo.ai>
2024-04-17 11:11:16 +0530

    Answer Entity added with Proper validations

---
+++ src/main/java/com/iamneo/springboot/LMS/model/Answer.java
@@ -0,0 +1,47 @@
+package com.iamneo.springboot.LMS.model;
+
+
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long answerId;
+
+    private String studentId;
+    private int marks;
+
+    @JsonBackReference
+    @OneToOne
+    private Question question;
+
+    @JsonBackReference
+    @OneToMany
+    private List<Choice> answers;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "test", referencedColumnName = "testId")
+    private Test test;
+}
