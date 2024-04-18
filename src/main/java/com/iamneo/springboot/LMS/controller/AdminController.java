028a0d791ceacb622c6358528edb485dfd4de103
karthikeya <venkata.karthikeya@iamneo.ai>
2024-04-18 15:31:02 +0530

    REVIEWED AND RESOLVED CONFLICTS
    Co-Authored-By: Srijan Airon <163994500+srijanNeo@users.noreply.github.com>

--- src/main/java/com/iamneo/springboot/LMS/controller/AdminController.java
+++ src/main/java/com/iamneo/springboot/LMS/controller/AdminController.java
@@ -1,72 +1,7 @@
-package com.iamneo.springboot.LMS.controller;
+package com.iamneo.springboot.LMS.Controller;
 
-import com.iamneo.springboot.LMS.dto.request.RegisterRequest;
-import com.iamneo.springboot.LMS.dto.response.BasicResponse;
-import com.iamneo.springboot.LMS.enumerated.Role;
-import com.iamneo.springboot.LMS.model.User;
-import com.iamneo.springboot.LMS.service.AdminService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/lms/admin")
-@RequiredArgsConstructor
-@Validated
 public class AdminController {
-
-    private final AdminService adminService;
-
-    @PostMapping("/register/student")
-    public ResponseEntity<BasicResponse<String>> registerStudent(@Valid @RequestBody RegisterRequest registerRequest) {
-        BasicResponse<String> response = new BasicResponse<>();
-        try {
-            response = adminService.register(registerRequest, Role.STUDENT);
-            return new ResponseEntity<>(response, HttpStatus.CREATED);
-        } catch (Exception e) {
-            response.setMessage("Something went wrong");
-            response.setData(e.getMessage());
-            return new ResponseEntity<>(response, HttpStatus.EXPECTATION_FAILED);
-        }
-    }
-
-    @PostMapping("/register/teacher")
-    public ResponseEntity<BasicResponse<String>> registerTeacher(@Valid @RequestBody RegisterRequest registerRequest) {
-        BasicResponse<String> response = new BasicResponse<>();
-        try {
-            response = adminService.register(registerRequest, Role.TEACHER);
-            return new ResponseEntity<>(response, HttpStatus.CREATED);
-        } catch (Exception e) {
-            response.setMessage("Something went wrong");
-            response.setData(e.getMessage());
-            return new ResponseEntity<>(response, HttpStatus.EXPECTATION_FAILED);
-        }
-    }
-
-    @GetMapping("/getAll/users")
-    public ResponseEntity<List<User>> getAllUsers() {
-        return new ResponseEntity<>(adminService.findAll(), HttpStatus.OK);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e) {
-        Map<String, String> errorMap = new HashMap<>();
-
-        e.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errorMap.put(fieldName, errorMessage);
-        });
-
-        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
-    }
+    
 }
+
--- src/main/java/com/iamneo/springboot/LMS/controller/AuthenticationController.java
+++ src/main/java/com/iamneo/springboot/LMS/controller/AuthenticationController.java
@@ -1,39 +1,5 @@
-package com.iamneo.springboot.LMS.controller;
+package com.iamneo.springboot.LMS.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.iamneo.springboot.LMS.dto.request.LoginRequest;
-import com.iamneo.springboot.LMS.dto.response.BasicResponse;
-import com.iamneo.springboot.LMS.dto.response.LoginResponse;
-import com.iamneo.springboot.LMS.service.AuthenticationService;
-
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
-@RestController
-@RequiredArgsConstructor
 public class AuthenticationController {
 
-    private final AuthenticationService authenticationService;
-
-    @PostMapping("/lms/login")
-    public ResponseEntity<BasicResponse<LoginResponse>> loginUser(@RequestBody LoginRequest loginRequest) {
-        BasicResponse<LoginResponse> response = new BasicResponse<>();
-        try {
-            response = authenticationService.login(loginRequest);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(
-                BasicResponse.<LoginResponse>builder()
-                            .message(e.getMessage())
-                            .data(null)     
-                            .build(),
-                HttpStatus.EXPECTATION_FAILED);
-        }
-    }
 }
