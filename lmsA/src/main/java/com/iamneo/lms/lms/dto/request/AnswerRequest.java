63a0d49137f0d8a8f6c0fe9bd60e3233caf5a0af
Srijan Airon <163994500+srijanNeo@users.noreply.github.com>
2024-04-18 12:35:11 +0530

    Update AnswerRequest.java

--- lmsA/src/main/java/com/iamneo/lms/lms/dto/request/AnswerRequest.java
+++ lmsA/src/main/java/com/iamneo/lms/lms/dto/request/AnswerRequest.java
@@ -16,18 +16,18 @@ import lombok.NoArgsConstructor;
 public class AnswerRequest {
 
     @NotBlank(message = "Question ID is a mandatory field")
-    @Schema(name = "Question ID", example = "12345", description = "The unique identifier of the question")
+    @Schema(name = "Question ID", example = "12345", description = "The unique identifier of the question, this will be given automatically from session storage in front end")
     private long questionId;
 
     @NotBlank(message = "Student ID is a mandatory field")
-    @Schema(name = "Student ID", example = "S123", description = "The unique identifier of the student")
+    @Schema(name = "Student ID", example = "S123", description = "The unique identifier of the student, this will be given automatically from session storage in front end")
     private String studentId;
 
     @NotBlank(message = "Test ID is a mandatory field")
-    @Schema(name = "Test ID", example = "54321", description = "The unique identifier of the test")
+    @Schema(name = "Test ID", example = "54321", description = "The unique identifier of the test, this will be given automatically from session storage in front end")
     private long testId;
 
     @Size(min = 1, max = 4, message = "Please provide correct options of what the students have selected")
-    @Schema(name = "Answers", description = "List of selected options by the student", minItems = 1, maxItems = 4)
+    @Schema(name = "Answers", description = "List of selected options by the student", minItems = 1, maxItems = 4, this will be given automatically from session storage in front end)
     private List<Long> answers;
 }
